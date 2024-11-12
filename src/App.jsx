3c7f96d3acd3d0aad0e698e94af4c4787c5ddb73
Pratik Majage <89234809+PratikRameshMajage@users.noreply.github.com>
2024-11-12 09:15:06 +0530

    v24.11.07

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Homepage from "./components/Homepage";
+
+const App = () => {
+  return (
+    <div className="main h-screen w-screen bg-zinc-900 text-black font-[helvatica]">
+        <Homepage />    
+    </div>
+  );
+};
+
+export default App;
